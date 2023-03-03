0d86cd2fdf2c4b1f59ef32c3313d004fe95292cd
xihan123 <srxqzxs@vip.qq.com>
2023-03-03 17:46:27 +0800

    chore: 更新依赖库
    Signed-off-by: xihan123 <srxqzxs@vip.qq.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,20 @@
 buildscript {
-    val appVersionName by extra("2.0.4")
-    val appVersionCode by extra(204)
+    val appVersionName by extra("2.0.5")
+    val appVersionCode by extra(205)
     val minSdkVersion by extra(26)
     val targetSdkVersion by extra(33)
     val coreVersion by extra("1.9.0")
     val appcompatVersion by extra("1.7.0-alpha02")
-    val materialVersion by extra("1.9.0-alpha01")
-    val YuKiHookVersion by extra("1.1.8")
-    val accompanistVersion by extra("0.28.0")
+    val materialVersion by extra("1.9.0-alpha02")
+    val yuKiHookVersion by extra("1.1.8")
+    val accompanistVersion by extra("0.29.1-alpha")
     val activityVersion by extra("1.8.0-alpha01")
     val navComposeVersion by extra("2.6.0-alpha05")
     val fastJson2Version by extra("2.0.24") //2.0.21.android
-    val kotlinxJsonVersion by extra("1.4.1")
+    val kotlinxJsonVersion by extra("1.5.0")
     val coilVersion by extra("2.2.2")
-    val landscapistVersion by extra("2.1.3")
-    val kotlinCompilerExtensionVersion by extra("1.4.1-dev-k1.8.10-c312d77f4cb")
+    val landscapistVersion by extra("2.1.4")
+    val kotlinCompilerExtensionVersion by extra("1.4.2")
 }
 plugins {
     id("com.android.application") apply false
