5185e83c54be94265ddedd01c8e0957e3c465c86
xihan123 <srxqzxs@vip.qq.com>
2023-07-31 17:19:14 +0800

    chore: 更新依赖
    Signed-off-by: xihan123 <srxqzxs@vip.qq.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
+buildscript {
+    val appVersionName by extra("2.1.9")
+    val appVersionCode by extra(219)
+}
 @Suppress("DSL_SCOPE_VIOLATION") // TODO: Remove once KTIJ-19369 is fixed
 plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.kotlinAndroid) apply false
-    alias(libs.plugins.kotlinSerialization) apply false
-    alias(libs.plugins.ksp) apply false
-    alias(libs.plugins.jgit) apply false
+    alias(libs.plugins.com.android.application) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.org.jetbrains.kotlin.plugin.serialization) apply false
+    alias(libs.plugins.com.google.devtools.ksp) apply false
 }
 true // Needed to make the Suppress annotation work for the plugins block
