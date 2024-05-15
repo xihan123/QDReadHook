945d55ed1d51d5bdf0ecba3667da166180c1eace
xihan123 <srxqzxs@vip.qq.com>
2024-05-15 23:00:40 +0800

    chore: 更新依赖库
    Signed-off-by: xihan123 <srxqzxs@vip.qq.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,5 @@
 pluginManagement {
     repositories {
-//        maven("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/")
         google {
             content {
                 includeGroupByRegex("com\\.android.*")
@@ -15,7 +14,6 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-//        maven("https://mirrors.cloud.tencent.com/nexus/repository/maven-public/")
         google()
         mavenCentral()
         maven("https://jitpack.io"){
